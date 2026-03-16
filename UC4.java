fffb9a63af070e0ac28ab8b2b227cfb621abd9cf
gamerProg <gamer.dan444@gmail.com>
2026-03-16 16:43:20 +0530

    Create UC4.java

---
+++ UC4.java
@@ -0,0 +1,28 @@
+/**
+ * @author Snehil
+ * @version 4.0
+ */
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+
+        String original = "level";
+        String reversed = "";
+
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        System.out.println("input : " + original);
+        if (original.equals(reversed)) {
+            System.out.println("is a Palindrome? : true");
+        } else {
+            System.out.println("is a Palindrome? : false");
+        }
+            }
+        }
